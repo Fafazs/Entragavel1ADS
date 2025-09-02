5f42a97e4d6134d16ca1ab53c28324b3750f0911
Fabricio Nascimento <153478556+Fafazs@users.noreply.github.com>
2025-09-02 18:11:29 -0300

    Update contagemInt.js

--- algoritimo1ADS/javascript/contagemInt.js
+++ algoritimo1ADS/javascript/contagemInt.js
@@ -1,13 +1,21 @@
 //Questão 6
-//Dado um conjunto de dados retorne os valores inteiros entre o primeiro elemento (inclusive) e o N-ésimo (inclusive)
+//Dado um conjunto de dados, retorne os valores inteiros entre o primeiro elemento, e o valor N.
 
-function contarInt(dataset) {
+function contarValoresInteiros(dataset, N) {
+    let limite;
+    if (N > dataset.length) {
+        limite = dataset.length;
+    } else if (N < 1) {
+        return 0;
+    } else {
+        limite = N;
+    }
+    
     const inicio = dataset[0];
-    const fim = dataset.length;
-
     let count = 0;
     for (let i = 0; i < dataset.length; i++) {
-        if (Number.isInteger(dataset[i]) && dataset[i] >= inicio && dataset[i] <= fim) {
+        const valor = dataset[i];
+        if (Number.isInteger(valor) && valor >= inicio && valor <= limite) {
             count++;
         }
     }
@@ -15,6 +23,9 @@ function contarInt(dataset) {
     return count;
 }
 
-// Exemplo de uso:
-const dados = [2, 3.5, 4, 5, 6, "texto", 7, 8.2, 9, 10];
-console.log("Quantidade de valores inteiros é:", contarInt(dados));
+const dados = [2, 3.5, 4, 5, 6, "texto", 7, 8.2, 9, 10, 20, 30, "text", "ola", 33.3, 100,22,55,66,77,88];
+
+console.log(contarValoresInteiros(dados, 20));
+console.log(contarValoresInteiros(dados, 6));  
+console.log(contarValoresInteiros(dados, 10));  
+console.log(contarValoresInteiros(dados, 0));  
