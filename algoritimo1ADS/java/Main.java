98d7d5b72164202763a4ef84265f01db42fce0f1
Fabricio Nascimento <153478556+Fafazs@users.noreply.github.com>
2025-09-02 17:47:22 -0300

    Update Main.java

--- algoritimo1ADS/java/Main.java
+++ algoritimo1ADS/java/Main.java
@@ -5,15 +5,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Questão 1 – Números Primos
-        //Primo p = new Primo();
-        //int[] numerosParaTestar = {1, 2, 3, 4, 5, 8, 10, 12, 16, 17, 19, 20, 23, 24, 29, 31, 37, 41};
-        //p.verificarPrimos(numerosParaTestar);
+        Primo p = new Primo();
+        int[] numerosParaTestar = {1, 2, 3, 4, 5, 8, 10, 12, 16, 17, 19, 20, 23, 24, 29, 31, 37, 41};
+        p.verificarPrimos(numerosParaTestar);
 
         // Questão 2 – Somatório
-        //Somatorio s = new Somatorio();
-        //int[] numerosParaSomar = {10, 20, 30, 40, 50};
-        //int resultado = s.somar(numerosParaSomar);
-        //System.out.println("Somatório: " + resultado);
+        Somatorio s = new Somatorio();
+        int[] numerosParaSomar = {10, 20, 30, 40, 50};
+        int resultado = s.somar(numerosParaSomar);
+        System.out.println("Somatório: " + resultado);
 
         // Questão 3 – Fibonacci
         System.out.print("Digite quantos números da sequência de Fibonacci deseja ver: ");
@@ -29,5 +29,17 @@ public class Main {
         System.out.println();
 
         scanner.close();
+
+        //Questão 4
+        Mdc md = new Mdc();
+        int a = 56, b = 98;
+        System.out.println("MDC de " + a + " e " + b + " é: " + md.mdc(a, b));
+
+        //Questão 5
+        QuickSort qs = new QuickSort;
+        int[] arr = {10, 5, 3, 8, 2, 9};
+        int[] sortedArr = qs.quickSort(arr);
+        System.out.println("Array ordenado: " + Arrays.toString(sortedArr));
     }
 }
+
